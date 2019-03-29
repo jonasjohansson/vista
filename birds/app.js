5aedc2f0ef1c67ebd4a8d52c36cab86ffdfcc0af
Jonas Johansson <jns.johansson@gmail.com>
2019-03-29 19:54:45 +0100

    Update app.js

--- birds/app.js
+++ birds/app.js
@@ -148,3 +148,13 @@ function render() {
 
 	renderer.render(scene, camera);
 }
+
+window.addEventListener(
+	'resize',
+	function(event) {
+		camera.aspect = window.innerWidth / window.innerHeight;
+		camera.updateProjectionMatrix();
+		renderer.setSize(window.innerWidth, window.innerHeight);
+	},
+	false
+);
